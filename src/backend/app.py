1e457231248430e2c849d14f85cb9dc39adc8e9e
manip_ego <52987267+manipEgo@users.noreply.github.com>
2023-05-20 18:05:14 +0800

    feat: backend invoke pipeline

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, Response
+from ..pipline import Pipeline
+import numpy as np
+import cv2
+
+checkpoint = "/home/mpyg/Documents/Codes/MIP_SAM/model/sam_vit_h_4b8939.pth"
+model_type = "vit_h"
 
 app = Flask(__name__)
+pipeline = Pipeline(checkpoint, model_type)
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -17,6 +24,16 @@ def upload_image():
 @app.route("/img/<imageId>.png")
 def get_frame(imageId):
     with open(r'../img/{}.png'.format(imageId), 'rb') as f: 	
+        image = f.read()
+        resp = Response(image, mimetype="image/png")
+        return resp
+
+@app.route('/process/<imageId>.png', methods=['POST'])
+def proceed_image(imageId):
+    image = cv2.imread(r'../img/{}.png'.format(imageId))
+    image =  pipeline.pipeline(image)
+    cv2.imwrite(r'../img/{}_sam.png'.format(imageId), image)
+    with open(r'../img/{}_sam.png'.format(imageId), 'rb') as f: 	
         image = f.read()
         resp = Response(image, mimetype="image/png")
         return resp
